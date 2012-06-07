5bf8b45e6d7751671a588fcc6f8ba56fbbc0da5f
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 16:13:39 +0200

    Removed "v" from cookbook license string in metadata.
    To be in line with all the other cookbooks.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Thorsten Fischer"
 maintainer_email "thorsten@froschi.org"
-license          "Apache v2.0"
+license          "Apache 2.0"
 description      "Installs/Configures libcomerr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
